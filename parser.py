0e7e351b0dc5f49743ec8fabd4cb813b1197a257
Aryan Naraghi <aryan.naraghi@gmail.com>
2020-06-27 16:26:26 -0700

    Makes the parser more robust.

--- parser.py
+++ parser.py
@@ -1,17 +1,33 @@
-import collections
 import enum
 import json
 import logging
+import urllib.parse
 
 
+# Source: https://en.wikipedia.org/wiki/Seattle_Fire_Department
+#
+# TODO(aryann): Many of the unit types are missing from this list.
 UNIT_MAP = {
-    'A': 'Aid Unit',
+    'A': 'Basic Life Support',
+    'AIR': 'Air Unit',
     'B': 'Battalion Chief',
+    'CHAP': 'Chaplain Unit',
+    'COM': 'Communication Unit',
+    'DECON': 'Decontamination Unit',
+    'DEP': 'Deputy Chief',
     'E': 'Engine',
+    'FB': 'Fire Boat',
+    'FRB': 'Fire Rescue Boat',
+    'HAZ': 'HAZMAT Unit',
+    'HOSE': 'Hose/Foam Wagon',
     'ICS': 'Incident Command System',
     'L': 'Ladder',
-    'M': 'Medic Unit',
+    'M': 'Advanced Life Support',
+    'MAR': 'Fire Marshall',
+    'MARINE': 'Marine Unit',
     'R': 'Technical Rescue Unit',
+    'REHAB': 'Rehabilitation',
+    'SAFT': 'Safety Chief',
     'STAF': 'Support Unit',
 }
 
@@ -38,21 +54,30 @@ def process_location(location):
     return location.replace('/', 'and').upper()
 
 
+def get_unit_type(unit):
+    for i, char in enumerate(unit):
+        if char.isdigit():
+            return unit[:i]
+    return unit
+
+
 def process_units(units_data):
-    # TODO(aryann): Handle unexpected unit types.
     units = []
     for unit in units_data.split():
-        unit_type = UNIT_MAP.get(unit[: 1])
-        if len(unit) < 2 or not unit_type or not unit[1].isdigit():
-            logging.warning('Encountered unknown unit type: %s', units_data)
+        unit_type = UNIT_MAP.get(get_unit_type(unit))
+        if not unit_type:
+            logging.warning(
+                'Encountered unknown unit type "%s"; data line: %s', unit, units_data)
             continue
         units.append(unit_type + ' ' + unit[1:])
 
     units.sort()
     if not units:
         return []
-    if len(units) == 1:
+    elif len(units) == 1:
         return units[0]
+    elif len(units) == 2:
+        return f'{units[0]} and {units[1]}'
     else:
         units[-1] = f'and {units[-1]}'
         return ', '.join(units)
@@ -86,7 +111,10 @@ def get_incidents(lines):
             state = ParsingState.EXPECT_LOCATION
 
         elif state == ParsingState.EXPECT_LOCATION:
-            curr['location'] = process_location(extract_cell_data(line))
+            location = process_location(extract_cell_data(line))
+            curr['location'] = location
+            map_query = urllib.parse.quote(location)
+            curr['map_link'] = f'https://www.google.com/maps/search/?api=1&query={map_query}'
             state = ParsingState.EXPECT_TYPE
 
         elif state == ParsingState.EXPECT_TYPE:
